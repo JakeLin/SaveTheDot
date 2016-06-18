b081e9b48901d89e5f4da94fbd976a400fe85ad3
Jake Lin <JakeLinAu@gmail.com>
2016-06-18 23:30:46 +1000

    Initial Commit

---
+++ SaveTheDot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SaveTheDot
+//
+//  Created by Jake Lin on 6/18/16.
+//  Copyright © 2016 Jake Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
